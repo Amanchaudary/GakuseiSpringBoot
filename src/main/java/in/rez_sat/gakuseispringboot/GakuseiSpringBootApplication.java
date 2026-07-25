d5fa996c9af657e60ca9ea93ba47fdffb695023d
チャウダリアマン <amanecdy@gmail.com>
2026-07-25 10:05:59 +0900

    sat project

---
+++ src/main/java/in/rez_sat/gakuseispringboot/GakuseiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package in.rez_sat.gakuseispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GakuseiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GakuseiSpringBootApplication.class, args);
+        System.out.println("aman");
+    }
+}
---
+++ src/test/java/in/rez_sat/gakuseispringboot/GakuseiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rez_sat.gakuseispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GakuseiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
